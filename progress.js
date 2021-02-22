cecfd68e53463af55734cda61afd84520229bba4
Florian Toribio <florian.toribio@etu.unilim.fr>
2021-02-22 13:30:07 +0100

    v1.2

--- progress.js
+++ progress.js
@@ -18,13 +18,28 @@ $(function () {
         //déclancher une animation ou pas
         let animation = parseInt($(this).data("animation"))
         if (animation > 0){
-            $(this).css('clip-path', `inset(0% 100% 0% 0%)`).animate({someValue: parseInt($(this).data("percent"))}, {
-                duration: animation,
-                step: function(someValue) {$(this).css('clip-path', `inset(0% ${100-someValue}% 0% 0%)`)}
-            });
+            if ($(this).offset().top > $(window).scrollTop() && $(this).offset().top < $(window).scrollTop()+$(window).height()) anim($(this));
+            else $(this).addClass("animationProgress");
         } else {
             $(this).css('clip-path', `inset(0% ${100-parseInt($(this).data("percent"))}% 0% 0%)`);
         }
 
     });
+
+    function anim(elem){
+        $(elem).css('clip-path', `inset(0% 100% 0% 0%)`).animate({someValue: parseInt($(elem).data("percent"))}, {
+            duration: parseInt($(elem).data("animation")),
+            step: function(someValue) {$(elem).css('clip-path', `inset(0% ${100-someValue}% 0% 0%)`)}
+        }).delay(800);
+    }
+
+    $(window).scroll(function () {
+        $('.animationProgress').each(function(){
+            if ($(this).offset().top > $(window).scrollTop() && $(this).offset().top < $(window).scrollTop()+$(window).height()){
+                anim($(this));
+                $(this).removeAttr('class', 'animationProgress');
+            }
+        })
+    });
+
 })
